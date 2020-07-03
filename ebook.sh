e2153078b6f2c5ee97fd40d9999205015f087373
AnimMouse <git@animmouse.eu.org>
2020-07-03 21:49:39 +0800

    Fix script

--- ebook.sh
+++ ebook.sh
@@ -15,18 +15,18 @@ cat frontmatter.md README.md >> hacker-laws.md
 
 # Remove the title - we have it in the front-matter of the doc, so it will
 # automatically be added to the PDF.
-sed -i '' '/💻📖.*/d' hacker-laws.md
+sed -i '/💻📖.*/d' hacker-laws.md
 
 # We can't have emojis in the final content with the PDF generator we're using.
-sed -i '' 's/❗/Warning/' hacker-laws.md
+sed -i 's/❗/Warning/' hacker-laws.md
 
 # Now rip out the translations line.
-sed -i '' '/^\[Translations.*/d' hacker-laws.md
+sed -i '/^\[Translations.*/d' hacker-laws.md
 
 # # Now rip out any table of contents items.
-sed -i '' '/\*.*/d' hacker-laws.md
-sed -i '' '/    \*.*/d' hacker-laws.md
+sed -i '/\*.*/d' hacker-laws.md
+sed -i '/    \*.*/d' hacker-laws.md
 
 # Delete everything from 'Translations' onwards (we don't need the translations
 # lists, related projects, etc).
-sed -i ' ' '/## Translations/,$d' hacker-laws.md
+sed -i '/## Translations/,$d' hacker-laws.md
